8277867dd5f94fcfaa360ba6729876971caf41ec
throughly410 <challengethroughyl@gmail.com>
2020-10-06 14:40:00 +0800

    增加共用層

---
+++ PET_ADOPTION_SYSTEM.Common/RandomTool.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace PET_ADOPTION_SYSTEM.Common
+{
+    public class RandomTool
+    {
+        /// <summary>
+        /// 取得GUID隨機字串
+        /// </summary>
+        /// <returns></returns>
+        public static string GetGuidString()
+        {
+            return Guid.NewGuid().ToString("N");
+        }
+    }
+}
